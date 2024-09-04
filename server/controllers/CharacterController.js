645fa0f9e820b551650872b2def26c0f6e1f9fdf
egahandayani <ega.handayani07@gmail.com>
2024-09-04 17:23:24 +0700

    feat: CRUD Done

--- server/controllers/CharacterController.js
+++ server/controllers/CharacterController.js
@@ -1,5 +1,13 @@
-import axios from "axios";
+const axios = require("axios");
 const { MyCharacter } = require("../models");
+const mycharacter = require("../models/mycharacter");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 module.exports = class CharacterController {
   static async getCharacters(req, res, next) {
     try {
@@ -40,15 +48,11 @@ module.exports = class CharacterController {
   }
 
   static async getOneCharacterById(req, res, next) {
-    const { id } = req.params;
+    const { _id } = req.params;
     try {
-      const apiUrl = `https://api.disneyapi.dev/character/${id}`;
+      const apiUrl = `https://api.disneyapi.dev/character/${_id}`;
       const response = await axios.get(apiUrl);
       const character = response.data.data[0];
-
-      if (!character) {
-        return res.status(404).json({ message: "Character not found" });
-      }
 
       const result = {
         id: character._id,
@@ -81,41 +85,17 @@ module.exports = class CharacterController {
   }
 
   static async addMyCharacter(req, res, next) {
-    const { UserId, CharacterId } = req.body;
     try {
-      const newMyCharacter = await MyCharacter.create({ UserId, CharacterId });
-
-      const apiUrl = `https://api.disneyapi.dev/character/${CharacterId}`;
-      const response = await axios.get(apiUrl);
-      const character = response.data.data[0];
-
-      const result = {
-        id: character._id,
-        films: character.films.length > 0 ? character.films : null,
-        shortFilms:
-          character.shortFilms.length > 0 ? character.shortFilms : null,
-        tvShows: character.tvShows.length > 0 ? character.tvShows : null,
-        videoGames:
-          character.videoGames.length > 0 ? character.videoGames : null,
-        parkAttractions:
-          character.parkAttractions.length > 0
-            ? character.parkAttractions
-            : null,
-        allies: character.allies.length > 0 ? character.allies : null,
-        enemies: character.enemies.length > 0 ? character.enemies : null,
-        sourceUrl: character.sourceUrl,
-        name: character.name,
-        imageUrl: character.imageUrl,
-        createdAt: character.createdAt,
-        updatedAt: character.updatedAt,
-        url: character.url,
-        version: character.__v,
-      };
-
-      res.status(201).json({
-        message: "MyCharacter added successfully",
-        myCharacter: result,
+      const response = await axios.get(
+        `https://api.disneyapi.dev/character/${req.params.CharacterId}`
+      );
+      const character = response.data.data;
+      const myCharacter = await MyCharacter.create({
+        UserId: req.user.id,
+        CharacterId: req.params.CharacterId,
+        imageUrl: character.imageUrl || null,
       });
+      res.status(200).json(myCharacter);
     } catch (err) {
       console.log("🚀 ~ CharacterController ~ addMyCharacter ~ err:", err);
       next(err);
@@ -167,28 +147,69 @@ module.exports = class CharacterController {
   }
 
   static async updateMyCharacter(req, res, next) {
-    const { id } = req.params;
-    const { imageUrl } = req.body;
-    const UserId = req.user.id;
-
     try {
-      const character = await MyCharacter.findOne();
+      const id = req.params.id;
+      const userId = req.user.id;
 
-      if (!character) {
-        return res.status(404).json({ message: "Character not found" });
+      const myCharacter = await MyCharacter.findOne({
+        where: { UserId: userId, id: id },
+      });
+
+      if (!myCharacter) {
+        throw {
+          name: "NotFound",
+          message: `MyCharacter with ID: ${id} not found`,
+        };
       }
 
-      let finalImageUrl = imageUrl;
-      if (!finalImageUrl) {
-        const response = await axios.get(
-          `https://api.disneyapi.dev/character/${id}`
-        );
-        finalImageUrl = response.data.data[0].imageUrl || character.imageUrl;
-      } else {
-        
+      if (!req.file) {
+        throw {
+          name: "BadRequest",
+          message: "No image file uploaded",
+        };
       }
+
+      const mimeType = req.file.mimetype;
+      const base64Image = req.file.buffer.toString("base64");
+
+      const result = await cloudinary.uploader.upload(
+        `data:${mimeType};base64,${base64Image}`,
+        {
+          folder: "disney_characters",
+          public_id: req.file.originalname,
+        }
+      );
+
+      await myCharacter.update({ imageUrl: result.secure_url });
+
+      res.json({ message: "Character image has been updated" });
     } catch (err) {
       console.log("🚀 ~ CharacterController ~ updateMyCharacter ~ err:", err);
+      next(err);
+    }
+  }
+
+  static async deleteMyCharacter(req, res, next) {
+    try {
+      const { id } = req.params;
+      const userId = req.user.id;
+
+      const myCharacter = await MyCharacter.findOne({
+        where: { id: id, UserId: userId },
+      });
+
+      if (!myCharacter) {
+        return res
+          .status(404)
+          .json({ message: "Character not found in myCharacters." });
+      }
+
+      await myCharacter.destroy();
+      res.status(200).json({
+        message: "Character successfully removed from myCharacters.",
+      });
+    } catch (err) {
+      console.log("🚀 ~ CharacterController ~ deleteMyCharacter ~ err:", err);
       next(err);
     }
   }
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 const authentication = require("../middlewares/authentication");
 const UserController = require("../controllers/UserController");
 const CharacterController = require("../controllers/CharacterController");
-const { route } = require("../app");
+const multer = require("multer");
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/", (req, res, next) => {
   res.send(`
@@ -18,9 +19,13 @@ router.post("/login/google", UserController.googleLogin);
 router.use(authentication);
 router.get("/characters", CharacterController.getCharacters);
 router.get("/characters/:id", CharacterController.getOneCharacterById);
-router.post("/characters/myCharacter", CharacterController.addMyCharacter);
+router.post("/characters/:CharacterId", CharacterController.addMyCharacter);
 router.get("/characters/myCharacters", CharacterController.getMyCharacter);
-
-// router.get("/character");
+router.patch(
+  "/characters/:id/imageUrl",
+  upload.single("imageUrl"),
+  CharacterController.updateMyCharacter
+);
+router.delete("/characters/:id", CharacterController.deleteMyCharacter);
 
 module.exports = router;
